d9ee74e9c8048c4fe3ddd0ea1db1a66242cf9b1e
Shimork04 <mauryashivampersbiz@gmail.com>
2024-09-21 00:00:05 +0530

    first commit

---
+++ models/ChatRoom.java
@@ -0,0 +1,66 @@
+package models;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class ChatRoom {
+    private String roomId;
+    private ArrayList<String> activeUsers;
+    private ArrayList<String> messageHistory;
+    private File logFile;
+
+    public ChatRoom(String roomId) {
+        this.roomId = roomId;
+        this.activeUsers = new ArrayList<>();
+        this.messageHistory = new ArrayList<>();
+        this.logFile = new File(roomId + "_chat_log.txt");  // Create a unique log file for the room
+        loadMessageHistory();  // Load message history when chat room is created
+    }
+
+    // Add a user to the active users list
+    public void addUser(String username) {
+        activeUsers.add(username);
+    }
+
+    // Remove a user from the active users list
+    public void removeUser(String username) {
+        activeUsers.remove(username);
+    }
+
+    // Get the list of active users
+    public ArrayList<String> getActiveUsers() {
+        return activeUsers;
+    }
+
+    // Add a message to the chat history
+    public void addMessage(String message) {
+        messageHistory.add(message);
+        appendMessageToFile(message);
+    }
+
+    // Load past messages from the log file when the chat room is opened
+    private void loadMessageHistory() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(logFile))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                messageHistory.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("No previous chat history found.");
+        }
+    }
+
+    // Append each new message to the log file
+    private void appendMessageToFile(String message) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true))) {
+            writer.write(message);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> getMessageHistory() {
+        return messageHistory;
+    }
+}
